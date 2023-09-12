c6a93a937783b2b1891c7b7f9da0e93e48198b2d
Rowland Adimoha <rowlives@gmail.com>
2023-09-12 17:28:34 +0100

    add protected routes

--- app/_components/NavMenu.tsx
+++ app/_components/NavMenu.tsx
@@ -1,30 +1,58 @@
 "use client";
-
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 
+const activeRoute =
+  "block px-4 py-2 text-blue-500 bg-blue-100 hover:bg-blue-200";
+const inactiveRoute = "block px-4 py-2 text-gray-500 hover:text-gray-700";
 function AuthButton() {
   const { data: session } = useSession();
   if (session) {
     return (
       <>
         Signed in as {session?.user?.name} <br />
-        <Link href="#" onClick={() => signOut()}>Sign out</Link>
+        <Link href="#" onClick={() => signOut()}>
+          Sign out
+        </Link>
       </>
     );
   }
   return (
     <>
       Not signed in <br />
-      <Link href="#"  onClick={() => signIn()}>Sign in</Link>
+      <Link href="#" onClick={() => signIn()}>
+        Sign in
+      </Link>
     </>
   );
 }
 
 export default function NavMenu() {
+  const pathname = usePathname();
   return (
     <div>
       <AuthButton />
+      <hr className="my-4" />
+      <ul>
+        <li>
+          <a
+            href="/"
+            className={pathname === "/" ? activeRoute : inactiveRoute}
+          >
+            Active Route
+          </a>
+        </li>
+        <li>
+          <a
+            href="/protected"
+            className={pathname === "/protected" ? activeRoute : inactiveRoute}
+          >
+            Inactive Route
+          </a>
+        </li>
+        {/* Add more <li> elements for other routes as needed */}
+      </ul>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
       ) : (
         <div> Not signed in </div>
       )}
-      <Button>Click me</Button>
+     
     </>
   );
 }
